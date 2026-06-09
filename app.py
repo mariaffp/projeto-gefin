0e7d410e8809a03384d8cc6a5776284168f06bcf
mariaffp <maria.ffernandes.p@gmail.com>
2026-06-09 18:11:19 -0300

    implementa autenticação completa com email/senha e OAuth Google, proteção de rotas

--- app.py
+++ app.py
@@ -17,12 +17,15 @@ app.layout = html.Div([
 ])
 @app.callback(
     Output("auth-check", "children"),
-    Input("url", "pathname")
+    Input("url", "pathname"),
+    Input("url", "search")
 )
-def verificar_autenticacao(pathname):
+def verificar_autenticacao(pathname,search):
     paginas_publicas = ["/", "/login"]
     if pathname in paginas_publicas:
         return ""
+    if search and "code=" in search:
+        return ""#dcc.Location(href="/", id="redirecionar-auth", refresh=True)
     session = supabase.auth.get_session()
     if session is None:
         return dcc.Location(href="/", id="redirecionar-auth", refresh=True)
