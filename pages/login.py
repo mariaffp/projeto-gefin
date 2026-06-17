9d79b884c3af4e8374036ccdc83b4551263a851b
vicode360 <victor.s.iorio@gmail.com>
2026-06-17 18:41:09 -0300

    login animado

--- pages/login.py
+++ pages/login.py
@@ -15,148 +15,136 @@ COR_TEXTO_BRANCO = "#FFFFFF"
 COR_DESTAQUE = "#9EBFE8"
 COR_FUNDO_DIREITA = "#E4E4E4"
 
-layout = dbc.Container(
-    [dcc.Location(id="redirecionar", refresh=True),
-        dbc.Row(
+layout = html.Div(
+    [
+        dcc.Location(id="redirecionar", refresh=True),
+        dcc.Store(id="hcaptcha-token-store"),
+
+        # fundo com o blur
+        html.Div(
+            style={
+                "position": "fixed",
+                "top": 0,
+                "left": 0,
+                "width": "100vw",
+                "height": "100vh",
+                "background": "linear-gradient(to bottom right, rgba(109, 237, 255, 0.6), rgba(171, 195, 255, 0.6)), url('/assets/focusbackground.png')",
+                "backgroundSize": "cover",
+                "backgroundPosition": "center",
+                "filter": "blur(15px)",
+                "transform": "scale(1.1)",
+                "zIndex": -1,
+            }
+        ),
+
+        # camda principal
+        html.Div(
             [
-                # lado esquerdo apresentacao (ocupa 6 de 12 colunas)
-                dbc.Col(
+                # caixa do login
+                html.Div(
                     [
-                        html.Div(
-                            [   # doc HTML Elements: https://dash.plotly.com/dash-html-componen
-                                html.H1(
-                                    "BEM VINDO AO SISTEMA DE\nGERENCIAMENTO\nFINANCEIRO GEFIN",
-                                    style={
-                                        "whiteSpace": "pre-line",
-                                        "fontWeight": "bold",
-                                        "color": COR_TEXTO_BRANCO,
-                                        "fontSize": "2.5rem"
-                                    }
-                                ),
-                                html.Img(
-                                    src="/assets/gefin1.png",
-                                    style={
-                                        "marginTop": "80px",
-                                        "width": "480px",
-                                        "display": "block",
-                                        "margin": "60px auto 0 auto"
-                                    }
-                                )
-                            ],
-                            style={"maxWidth": "80%"}
-                        )
+                        html.Div([
+                            dbc.Label("E-mail", style={"fontWeight": "bold", "fontSize": "1.2rem"}),
+                            dbc.Input(
+                                id="input-email",
+                                type="email",
+                                placeholder="  Digite seu e-mail",
+                                className="mb-4 py-2",
+                                style={"borderRadius": "4px"}
+                            ),
+                            dbc.Label("Senha", style={"fontWeight": "bold", "fontSize": "1.2rem"}),
+                            dbc.Input(
+                                id="input-senha",
+                                type="password",
+                                placeholder="  Digite sua senha aqui",
+                                className="mb-4 py-2",
+                                style={"borderRadius": "4px"}
+                            ),
+                        ]),
+                        html.Div(id="msg-erro", className="text-danger mb-2"),
+                        html.Div([
+                            dbc.Button(
+                                "Acessar",
+                                id="btn-entrar",
+                                className="w-100 mb-3 py-2",
+                                style={
+                                    "backgroundColor": COR_BOTAO,
+                                    "borderColor": COR_BOTAO,
+                                    "color": COR_TEXTO_BRANCO,
+                                    "fontSize": "1.1rem"
+                                }
+                            ),
+                            html.Div(
+                                [
+                                    html.Hr(style={"flex": "1", "borderColor": "#666"}),
+                                    html.Span("OU", style={"padding": "0 15px", "fontSize": "1.1rem"}),
+                                    html.Hr(style={"flex": "1", "borderColor": "#666"}),
+                                ],
+                                className="d-flex align-items-center mb-3"
+                            ),
+                            dbc.Button(
+                                [
+                                    html.Img(
+                                        src="/assets/google.png",
+                                        style={
+                                            "width": "24px",
+                                            "marginRight": "10px",
+                                            "verticalAlign": "middle"
+                                        }
+                                    ),
+                                    html.Span("Acessar utilizando Google")
+                                ],
+                                id="btn-google",
+                                className="w-100",
+                                style={
+                                    "backgroundColor": COR_BOTAO,
+                                    "borderColor": COR_BOTAO,
+                                    "color": COR_TEXTO_BRANCO,
+                                    "fontSize": "1.1rem",
+                                    "display": "flex",
+                                    "alignItems": "center",
+                                    "justifyContent": "center",
+                                }
+                            )
+                        ])
                     ],
-                    md=6,
-                    className="d-flex flex-column justify-content-center align-items-center vh-100",
-                    style={"backgroundColor": COR_BOTAO}
+                    style={
+                        "backgroundColor": COR_FUNDO_DIREITA,
+                        "padding": "40px",
+                        "borderRadius": "12px",
+                        "boxShadow": "0px 15px 35px rgba(0, 0, 0, 0.5)",
+                        "width": "100%",
+                        "maxWidth": "450px",
+                        "marginLeft": "8vw",
+                        "zIndex": 2
+                    }
                 ),
 
-                # lado direito formulario (ocupa 6 de 12 colunas)
-                dbc.Col(
-                    [
-                        html.Div(
-                            [   # inputs de email e senha
-                                # doc de i  nputs: https://www.dash-bootstrap-components.com/docs/components/input/
-                                html.Div([
-                                    dbc.Label("E-mail", style={"fontWeight": "bold", "fontSize": "1.2rem"}),
-                                    dbc.Input(
-                                        id="input-email",
-                                        type="email",
-                                        placeholder="🧑🏻‍💻 Digite seu e-mail",
-                                        className="mb-4 py-2",
-                                        style={"borderRadius": "4px"}
-                                    ),
-                                    dbc.Label("Senha", style={"fontWeight": "bold", "fontSize": "1.2rem"}),
-                                    dbc.Input(
-                                        id="input-senha",
-                                        type="password",
-                                        placeholder="🔐 Digite sua senha aqui",
-                                        className="mb-4 py-2",
-                                        style={"borderRadius": "4px"}
-                                    ),
-                                ]),
-
-                                html.Div(id="msg-erro", className="text-danger mb-2"),
-
-                                html.Div([
-                                    dbc.Button(
-                                        "Acessar",
-                                        id="btn-entrar",
-                                        className="w-100 mb-3 py-2",
-                                        style={
-                                            "backgroundColor": COR_BOTAO,
-                                            "borderColor": COR_BOTAO,
-                                            "color": COR_TEXTO_BRANCO,
-                                            "fontSize": "1.1rem"
-                                        }
-                                    ),
-                                    # linha do "OU"
-                                    html.Div(
-                                        [
-                                            html.Hr(style={"flex": "1", "borderColor": "#666"}),
-                                            html.Span("OU", style={"padding": "0 15px", "fontSize": "1.1rem"}),
-                                            html.Hr(style={"flex": "1", "borderColor": "#666"}),
-                                        ],
-                                        className="d-flex align-items-center mb-3"
-                                    ),
-
-                                    dbc.Button(
-                                        [
-                                            html.Div(
-                                                html.Img(
-                                                    src="/assets/google.png",
-                                                    style={
-                                                        "display": "flex",
-                                                        "alignItems": "center",
-                                                        "marginRight": "10px",
-                                                        "width": "30px"
-                                                        }
-                                                ),
-                                                style={
-                                                    "padding": "1px",
-                                                    "borderRadius": "2px",
-                                                    "marginRight": "30px",
-                                                    "display": "inline-block",
-                                                    "position": "absolute",
-                                                    "top": "500px",
-                                                    "right": "520px"
-                                                }
-                                            ),
-                                            html.Span("Acessar utilizando Google")
-                                        ],
-                                        id="btn-google",
-                                        className="w-100",
-                                        style={
-                                            "backgroundColor": COR_BOTAO,
-                                            "borderColor": COR_BOTAO,
-                                            "color": COR_TEXTO_BRANCO,
-                                            "fontSize": "1.1rem",
-                                            "alignItems": "center",
-                                            "justifyContent": "center",
-                                            "gap": "15px"
-                                        }
-                                    )
-                                ])
-                            ],
-                            style={"width": "100%", "maxWidth": "450px"}
-                        )
-                    ],
-                    md=6,
-                    className="d-flex justify-content-center align-items-center vh-100",
-                    style={"backgroundColor": COR_FUNDO_DIREITA}
+                # logo animada svg
+                html.ObjectEl(
+                    data="/assets/focus_animatedlogo.svg",
+                    type="image/svg+xml",
+                    style={
+                        "width": "450px",
+                        "height": "450px",
+                        "marginRight": "8vw",
+                        "pointerEvents": "none",
+                        "zIndex": 0
+                    }
                 )
             ],
-            className="m-0"
+            # w-100 e justify-content-between organizam o espaço horizontalmente
+            className="d-flex align-items-center justify-content-between vh-100 w-100",
+            style={"position": "relative", "zIndex": 1}
         )
     ],
-    fluid=True,
-    className="p-0",
     style={"fontFamily": "'Trade Gothic Next SR Pro Regular', sans-serif"}
 )
 
 #callback do login, sempre que o usuario clicar no botao de acesso, o dash captura esses valores e executa a função
 @callback(
     Output("msg-erro", "children"),
+    Output("redirecionar-email", "pathname"),
     Input("btn-entrar", "n_clicks"),
     Input("input-email", "value"),
     Input("input-senha", "value"),
@@ -164,12 +152,12 @@ layout = dbc.Container(
 )
 def login_email(n_clicks, email, senha):
     if not n_clicks:
-        return ""
+        return "",dash.no_update
     try: #tenta enviar as credenciais pro supabase autenticar
         supabase.auth.sign_in_with_password({"email": email, "password": senha})
-        return ""
+        return "","/dashboard" #se der certo, redireciona para a pagina principal do sistema
     except Exception as e:
-        return "E-mail ou senha incorretos." #tratamento de erro, caso nao encontre o login
+        return "E-mail ou senha incorretos.", dash.no_update #tratamento de erro, caso nao encontre o login
 
 #callback responsável pelo login via google
 #quando o botao for clicado, o supabase ger uma URL de autenticacao e redireciona o usuario para ela
