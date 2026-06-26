2b671a854514688559d5b5ac933cf46530ee191e
João Victor Gabriel <jv140604@gmail.com>
2026-06-26 19:47:02 -0300

    lidando com problemas de diferenças de codigos

--- services/transacao.py
+++ services/transacao.py
@@ -1,15 +1,10 @@
 from supabase_client import supabase
 
 
-def listar_transacoes(id_categoria=None, tipo=None, periodo=None, id_conta=None,
-                       data_inicio=None, data_fim=None):
-    """
-    Busca transações aplicando filtros opcionais.
-    Qualquer filtro que não for informado (None) é ignorado.
-    """
-    query = supabase.table("transacao").select(
-        "*, categoria(nome), conta(nome), forma_pagamento(nome)"
-    )
+def listar_transacoes(id_categoria=None, tipo=None, periodo=None, id_conta=None, data_inicio=None, data_fim=None):
+    
+    #Busca transações aplicando filtros opcionais, qualquer filtro que não for informado (None) é ignorado.
+    query = supabase.table("transacao").select("*, categoria(nome), conta(nome), forma_pagamento(nome)")
 
     if id_categoria:
         query = query.eq("id_categoria", id_categoria)
